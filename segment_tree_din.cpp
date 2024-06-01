aef084b6784f79fdb0a9f24adcdbf7c03538ce19
Pedro Vinicius <pedroestouaqui@gmail.com>
2024-06-01 01:07:58 -0300

    Mudei um pouco a segment tree dinâmica

--- segment_tree_din.cpp
+++ segment_tree_din.cpp
@@ -4,47 +4,56 @@ struct item {
 
 void _merge(item &a, item &b, item &c) { a.val = b.val + c.val; }
 
-vector<item> seg;
-item neutro;
+int tamseg = 1e9 + 7;
 
-int _create() {
-  seg.emplace_back();
-  return seg.size() - 1;
-}
+struct Segtree {
+  vector<item> seg;
+  item neutro;
 
-void initseg() { _create(); }  // TODO: chamar antes de usar
-
-ll getseg(ll l, ll r, int pos, ll lx, ll rx) {
-  if (pos == -1 || lx >= r || rx <= l) return 0LL;
-  if (lx >= l && rx <= r) {
-    return seg[pos].val;
+  int _create() {
+    seg.emplace_back();
+    return seg.size() - 1;
   }
-  ll mid = lx + (rx - lx) / 2;
-  ll x = getseg(l, r, seg[pos].e, lx, mid);
-  ll y = getseg(l, r, seg[pos].d, mid, rx);
-  return x + y;
-}
 
-void addseg(ll i, int v, int pos, ll lx, ll rx) {
-  if (rx - lx == 1) {
-    seg[pos].val += v;
-    return;
-  }
-  ll mid = lx + (rx - lx) / 2;
-  if (i < mid) {
-    if (seg[pos].e == -1) {
-      int tmp = _create();
-      seg[pos].e = tmp;
+  Segtree() { _create(); }
+
+  ll getseg(ll l, ll r, int pos, ll lx, ll rx) {
+    if (lx >= r || rx <= l) return 0LL;
+    if (pos == -1) return 0LL;
+    if (lx >= l && rx <= r) {
+      return seg[pos].val;
     }
-    addseg(i, v, seg[pos].e, lx, mid);
-  } else {
-    if (seg[pos].d == -1) {
-      int tmp = _create();
-      seg[pos].d = tmp;
-    }
-    addseg(i, v, seg[pos].d, mid, rx);
+    ll mid = lx + (rx - lx) / 2;
+    ll x = getseg(l, r, seg[pos].e, lx, mid);
+    ll y = getseg(l, r, seg[pos].d, mid, rx);
+    return x + y;
   }
-  int e = seg[pos].e;
-  int d = seg[pos].d;
-  _merge(seg[pos], e == -1 ? neutro : seg[e], d == -1 ? neutro : seg[d]);
-}
+
+  int get(int l, int r) { return getseg(l, r, 0, -tamseg, tamseg); }
+
+  void addseg(ll i, int v, int pos, ll lx, ll rx) {
+    if (rx - lx == 1) {
+      seg[pos].val += v;
+      return;
+    }
+    ll mid = lx + (rx - lx) / 2;
+    if (i < mid) {
+      if (seg[pos].e == -1) {
+        int tmp = _create();
+        seg[pos].e = tmp;
+      }
+      addseg(i, v, seg[pos].e, lx, mid);
+    } else {
+      if (seg[pos].d == -1) {
+        int tmp = _create();
+        seg[pos].d = tmp;
+      }
+      addseg(i, v, seg[pos].d, mid, rx);
+    }
+    int e = seg[pos].e;
+    int d = seg[pos].d;
+    _merge(seg[pos], e == -1 ? neutro : seg[e], d == -1 ? neutro : seg[d]);
+  }
+
+  void set(int i, int v) { addseg(i, v, 0, -tamseg, tamseg); }
+};
