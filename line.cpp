3a4324594076ba40fb866074af386cd32e1b9334
Pedro Vinicius <pedroestouaqui@gmail.com>
2024-08-16 17:47:09 -0300

    Pequenas adições

--- line.cpp
+++ line.cpp
@@ -3,6 +3,7 @@ struct line {
   line() {}
   // a * x + b * y + c = 0
   line(int _a, int _b, int _c) : a(_a), b(_b), c(_c) {}
+  
   line(ii x, ii y) {
     a = (x.second - y.second);
     b = (-x.first + y.first);
@@ -13,10 +14,18 @@ struct line {
     b /= g;
     c /= g;
   }
+
   bool is(ii x) {
     int v1 = a * x.first;
     int v2 = b * x.second;
-    //       cout << v1 + v2 << " vs " << -c << "\n";
     return v1 + v2 == -c;
+  }
+
+  double gety(int x){
+    return ((-a * x - c) * (1.)) /  b;
+  }
+
+  double getx(int y){
+    return ((-b * y - c) * (1.)) /  a;
   }
 };
